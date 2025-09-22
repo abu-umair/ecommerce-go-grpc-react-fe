741fcdefb0fe12f452a8a001289bd63939f88eec
abu-umair <mrdwnw93@gmail.com>
2025-09-22 11:18:52 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Go gRpc FE - Ecommerce
+
+## E-Commerce Project - Setup
+### Instalasi Template E-Commerce ReactJS
+#### History Steps
+1. clone project https://github.com/Dryluigi/go-grpc-course-fe
+```bash
+git clone https://github.com/Dryluigi/go-grpc-course-fe.git
+
+```
+
+2. pastikan versi node 20
+```bash
+node -v
+
+```
+
+3. install dependency
+```bash
+npm i
+
+```
+
+4. run project
+```bash
+npm run dev
+
+```
+
+## E-Commerce Project - Autentikasi
+### Control Form FE
+#### History Steps
+
+
+### Reusable Input Component FE
+#### History Steps
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,60 @@
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+import Swal from 'sweetalert2';
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const loginSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+});
+
+interface LoginFormValues {
+    email: string;
+    password: string;
+}
+const Login = () => {
+    const form = useForm<LoginFormValues>({
+        resolver: yupResolver(loginSchema),
+    })
+    const submitHandler = (values: LoginFormValues) => {
+        console.log(values);
+        Swal.fire({
+            icon: 'success',
+            title: 'Login successfully',
+            confirmButtonText: 'Ok'
+        });
+    }
+    return (
+        <div className="login-section">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-6 col-lg-5">
+                        <div className="login-wrap p-4">
+                            <h2 className="section-title text-center mb-5">Masuk</h2>
+                            <form onSubmit={form.handleSubmit(submitHandler)} action="#" className="login-form">
+                                <div className="form-group mb-4">
+                                    <input type="text" className={`form-control ${form.formState.errors.email ? 'is-invalid' : ''}`} placeholder="Alamat Email" {...form.register('email')} />
+                                    <div className={`text-danger ${form.formState.errors.email ? '' : 'hidden'}`} style={{ height: 8 }}>{form.formState.errors.email?.message ?? ''}</div>
+                                </div>
+                                <div className="form-group mb-4">
+                                    <input type="password" className={`form-control ${form.formState.errors.password ? 'is-invalid' : ''}`} placeholder="Kata sandi" {...form.register('password')} />
+                                    <div className={`text-danger ${form.formState.errors.password ? '' : 'hidden'}`} style={{ height: 8 }}>{form.formState.errors.password?.message ?? ''}</div>
+                                </div>
+                                <div className="form-group">
+                                    <button type="submit" className="btn btn-primary btn-block">Masuk</button>
+                                </div>
+                                <div className="text-center mt-4">
+                                    <p>Belum punya akun? <Link to="/register" className="text-primary">Daftar di sini</Link>
+                                    </p>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
