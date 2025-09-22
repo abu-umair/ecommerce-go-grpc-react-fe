d7e927489339b58995a57bf6285161e60a91fd05
abu-umair <mrdwnw93@gmail.com>
2025-09-22 11:29:25 +0700

    + membuat interface: menambahkan type & placeholder

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 interface FormInputProps {
-    
+    type: "text" | "password" //? type nya bisa text atau password
+    placeholder?: string //?tidak wajib, (bisa dibuat optional)
 }
 const FormInput = (props: FormInputProps) => {
     return (
         <div className="form-group mb-4">
-            <input type="text" className={`form-control ${form.formState.errors.email ? 'is-invalid' : ''}`} placeholder="Alamat Email" {...form.register('email')} />
+            <input type={props.type} className={`form-control ${form.formState.errors.email ? 'is-invalid' : ''}`} placeholder={props.placeholder} {...form.register('email')} />
             <div className={`text-danger ${form.formState.errors.email ? '' : 'hidden'}`} style={{ height: 8 }}>{form.formState.errors.email?.message ?? ''}</div>
         </div>
     )
