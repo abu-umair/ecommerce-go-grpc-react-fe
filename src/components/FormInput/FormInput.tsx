e65fe81f56dc3996341887470111f69d3814fcab
abu-umair <mrdwnw93@gmail.com>
2025-09-22 11:58:06 +0700

    + membuat interface: menambahkan errors

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import { UseFormRegister } from 'react-hook-form'
+import { FieldErrors, Path, UseFormRegister } from 'react-hook-form'
 
 // eslint-disable-next-line
 interface FormInputProps<T extends Record<string, any>> { //?perlu di extend hingga menjadi Record 
     type: "text" | "password" //? type nya bisa text atau password
     placeholder?: string //?tidak wajib, (bisa dibuat optional)
     register: UseFormRegister<T>; //?membuat generik 
+    name: Path<T>;
+    errors: FieldErrors<T>;
 }
 
 // eslint-disable-next-line
 function FormInput<T extends Record<string, any>>(props: FormInputProps<T>) {
     return (
         <div className="form-group mb-4">
-            <input type={props.type} className={`form-control ${form.formState.errors.email ? 'is-invalid' : ''}`} placeholder={props.placeholder} {...form.register('email')} />
-            <div className={`text-danger ${form.formState.errors.email ? '' : 'hidden'}`} style={{ height: 8 }}>{form.formState.errors.email?.message ?? ''}</div>
+            <input type={props.type} className={`form-control ${props.errors[props.name] ? 'is-invalid' : ''}`} placeholder={props.placeholder} {...props.register(props.name)} />
+            <div className={`text-danger ${props.errors[props.name] ? '' : 'hidden'}`} style={{ height: 8 }}>
+                {props.errors[props.name]?.message as string | null ?? ''}
+            </div>
         </div>
     )
 }
