65a4c5532c354c3e611342361293bbdf099a0ea3
abu-umair <mrdwnw93@gmail.com>
2025-10-01 08:27:10 +0700

    + menambahkan FormInput

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,14 +1,15 @@
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import FormInput from '../../components/FormInput/FormInput';
 
 interface RegisterFormValues {
-  full_name: string;
-  email: string;
-  password: string;
-  password_confirmation: string;
+    full_name: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
 }
 const Register = () => {
-    useForm<RegisterFormValues>();
+    const form = useForm<RegisterFormValues>();
 
     return (
         <div className="login-section">
@@ -18,6 +19,35 @@ const Register = () => {
                         <div className="login-wrap p-4">
                             <h2 className="section-title text-center mb-5">Daftar</h2>
                             <form action="#" className="login-form">
+                                <FormInput<RegisterFormValues>
+                                    errors={form.formState.errors}
+                                    name='full_name'
+                                    register={form.register}
+                                    type='text'
+                                    placeholder='Nama Lengkap'
+                                />
+                                <FormInput<RegisterFormValues>
+                                    errors={form.formState.errors}
+                                    name='email'
+                                    register={form.register}
+                                    type='text'
+                                    placeholder='Alamat email'
+                                />
+                                <FormInput<RegisterFormValues>
+                                    errors={form.formState.errors}
+                                    name='password'
+                                    register={form.register}
+                                    type='password'
+                                    placeholder='kata sandi'
+                                />
+                                <FormInput<RegisterFormValues>
+                                    errors={form.formState.errors}
+                                    name='password_confirmation'
+                                    register={form.register}
+                                    type='password'
+                                    placeholder='konfirmasi kata sandi'
+                                />
+
                                 <div className="form-group mb-4">
                                     <input type="text" className="form-control" placeholder="Nama Lengkap" required />
                                 </div>
