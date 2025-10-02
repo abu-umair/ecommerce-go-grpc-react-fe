b3ea5c510d8a839713b18ad13d3e4ea1dee20115
abu-umair <mrdwnw93@gmail.com>
2025-10-02 08:31:18 +0700

    + membuat pengecekan kondisi

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,11 +2,14 @@ import React from 'react'
 import { Timestamp } from '../../pb/google/protobuf/timestamp'
 
 export const convertTimestampToDate = (timestamp: Timestamp | undefined) => {
-    const date = new Date(Number(timestamp?.seconds) * 1000);
-    
-    setMemberSince(date.toLocaleDateString('id-ID', {
+    if (!timestamp) {
+        return
+    }
+    const date = new Date(Number(timestamp.seconds) * 1000);
+
+    return date.toLocaleDateString('id-ID', {
         day: 'numeric',
         month: 'long',
         year: 'numeric'
-    }));
+    });
 }
