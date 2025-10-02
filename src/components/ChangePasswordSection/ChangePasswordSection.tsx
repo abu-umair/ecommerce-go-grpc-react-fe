ab7a4aae6957bf8fa34819bfb8a0f643acc17d72
abu-umair <mrdwnw93@gmail.com>
2025-10-02 14:43:59 +0700

    + membuat input reusable

--- src/components/ChangePasswordSection/ChangePasswordSection.tsx
+++ src/components/ChangePasswordSection/ChangePasswordSection.tsx
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import FormInput from "../FormInput/FormInput";
 
 interface ChangePasswordFormValues {
     current_password: string;
@@ -6,25 +7,55 @@ interface ChangePasswordFormValues {
     confirm_new_password: string;
 }
 
-function ChangePasswordSection {
+function ChangePasswordSection() {
     const form = useForm<ChangePasswordFormValues>();
-    
+
+    const submitHandler = (values: ChangePasswordFormValues) => {
+        console.log(values);
+    }
+
     return (
         <div className="p-4 p-lg-5 border bg-white">
             <h2 className="h3 mb-3 text-black">Ubah Kata Sandi</h2>
-            <div className="form-group">
-                <label className="text-black" htmlFor="current_password">Kata Sandi Saat Ini</label>
-                <input type="password" className="form-control" id="current_password" />
-            </div>
-            <div className="form-group">
-                <label className="text-black" htmlFor="new_password">Kata Sandi Baru</label>
-                <input type="password" className="form-control" id="new_password" />
-            </div>
-            <div className="form-group mb-3">
-                <label className="text-black" htmlFor="confirm_password">Konfirmasi Kata Sandi Baru</label>
-                <input type="password" className="form-control" id="confirm_password" />
-            </div>
-            <button className="btn btn-primary">Perbarui Kata Sandi</button>
+            <form onSubmit={form.handleSubmit(submitHandler)}>
+                <FormInput<ChangePasswordFormValues>
+                    errors={form.formState.errors}
+                    name="current_password"
+                    register={form.register}
+                    type="password"
+                    label="Kata Sandi Saat Ini"
+                />
+
+                <FormInput<ChangePasswordFormValues>
+                    errors={form.formState.errors}
+                    name="new_password"
+                    register={form.register}
+                    type="password"
+                    label="Kata Sandi Baru"
+                />
+
+                <FormInput<ChangePasswordFormValues>
+                    errors={form.formState.errors}
+                    name="confirm_new_password"
+                    register={form.register}
+                    type="password"
+                    label="Konfirmasi Kata Sandi Baru"
+                />
+
+                {/* <div className="form-group">
+                    <label className="text-black" htmlFor="current_password">Kata Sandi Saat Ini</label>
+                    <input type="password" className="form-control" id="current_password" />
+                </div>
+                <div className="form-group">
+                    <label className="text-black" htmlFor="new_password">Kata Sandi Baru</label>
+                    <input type="password" className="form-control" id="new_password" />
+                </div>
+                <div className="form-group mb-3">
+                    <label className="text-black" htmlFor="confirm_password">Konfirmasi Kata Sandi Baru</label>
+                    <input type="password" className="form-control" id="confirm_password" />
+                </div> */}
+                <button type="submit" className="btn btn-primary">Perbarui Kata Sandi</button>
+            </form>
         </div>
     )
 }
