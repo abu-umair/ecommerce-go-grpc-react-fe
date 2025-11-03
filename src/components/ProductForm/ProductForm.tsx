f8665b36322153e0bdadceedf17095e2c4773447
abu-umair <mrdwnw93@gmail.com>
2025-11-03 10:45:17 +0700

    + menambahkan submitHandler

--- src/components/ProductForm/ProductForm.tsx
+++ src/components/ProductForm/ProductForm.tsx
@@ -1,29 +1,49 @@
+import { useForm } from "react-hook-form";
+
+interface ProductFormValues {
+    name: string;
+    price: number;
+    description: string;
+    image: FileList;
+}
+
+
 function ProductForm() {
+    const form = useForm<ProductFormValues>();
+
+
+    const submitHandler = (values: ProductFormValues) => {
+        console.log(values);
+
+    }
+
     return (
         <div className="p-4 p-lg-5 border bg-white">
-            <div className="form-group mb-3">
-                <label className="text-black" htmlFor="product_name">Nama Produk <span className="text-danger">*</span></label>
-                <input type="text" className="form-control" id="product_name" placeholder="Nama Produk" />
-            </div>
+            <form onSubmit={form.handleSubmit(submitHandler)}>
+                <div className="form-group mb-3">
+                    <label className="text-black" htmlFor="product_name">Nama Produk <span className="text-danger">*</span></label>
+                    <input type="text" className="form-control" id="product_name" placeholder="Nama Produk" />
+                </div>
 
-            <div className="form-group mb-3">
-                <label className="text-black" htmlFor="product_price">Harga <span className="text-danger">*</span></label>
-                <input type="number" className="form-control" id="product_price" placeholder="Harga Produk" />
-            </div>
+                <div className="form-group mb-3">
+                    <label className="text-black" htmlFor="product_price">Harga <span className="text-danger">*</span></label>
+                    <input type="number" className="form-control" id="product_price" placeholder="Harga Produk" />
+                </div>
 
-            <div className="form-group mb-3">
-                <label className="text-black" htmlFor="product_image">Gambar Produk <span className="text-danger">*</span></label>
-                <input type="file" className="form-control" id="product_image" accept="image/*" />
-            </div>
+                <div className="form-group mb-3">
+                    <label className="text-black" htmlFor="product_image">Gambar Produk <span className="text-danger">*</span></label>
+                    <input type="file" className="form-control" id="product_image" accept="image/*" />
+                </div>
 
-            <div className="form-group mb-4">
-                <label className="text-black" htmlFor="product_description">Deskripsi</label>
-                <textarea className="form-control" id="product_description" rows={4} placeholder="Deskripsi produk..."></textarea>
-            </div>
+                <div className="form-group mb-4">
+                    <label className="text-black" htmlFor="product_description">Deskripsi</label>
+                    <textarea className="form-control" id="product_description" rows={4} placeholder="Deskripsi produk..."></textarea>
+                </div>
 
-            <div className="form-group">
-                <button className="btn btn-primary">Simpan Produk</button>
-            </div>
+                <div className="form-group">
+                    <button className="btn btn-primary">Simpan Produk</button>
+                </div>
+            </form>
         </div>
     )
 }
