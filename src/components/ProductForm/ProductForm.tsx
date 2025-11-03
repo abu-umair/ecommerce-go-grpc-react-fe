1f8f0cdb216b8eb6eed9e045f5a1df998e402a36
abu-umair <mrdwnw93@gmail.com>
2025-11-03 11:22:13 +0700

    + menambahkan validasi

--- src/components/ProductForm/ProductForm.tsx
+++ src/components/ProductForm/ProductForm.tsx
@@ -1,10 +1,26 @@
 import { useForm } from "react-hook-form";
 import FormInput from "../FormInput/FormInput";
+import * as yup from "yup";
+
+
+const changeProductSchema = yup.object().shape({
+    name: yup.string().required("Nama produk wajib diisi"),
+    price: yup.number().required("Harga produk wajib diisi").typeError("Harga produk tidak valid").moreThan(0, "Harga produk harus lebih dari 0"),
+    description: yup.string(),
+    image: yup.mixed<FileList>().required("Gambar produk wajib diisi")
+        .test("fileLength", "Gambar produk wajib diisi", (fileList) => {
+            return fileList.length > 0
+        })
+        .test("fileType", "Format gambar tidak valid", (fileList) => {
+            return fileList && fileList.length > 0 ? ["image/jpeg", "image/png"].includes(fileList[0].type) : true
+            //?jika nilainya lebih ari 0 maka true, selain itu seakan-akan aman (sehingga di triger adl test yang pertama, bkn ke 2 nya)
+        })
+})
 
 interface ProductFormValues {
     name: string;
     price: number;
-    description: string;
+    description?: string;
     image: FileList;
 }
 
@@ -30,7 +46,7 @@ function ProductForm() {
                     placeholder="Nama Produk"
                     labelRequired
                 />
-              
+
 
                 {/* sementara dibiarin karena terkait harga / curency  */}
                 <div className="form-group mb-3">
