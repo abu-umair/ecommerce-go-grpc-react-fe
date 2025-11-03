3f14cc7cdae0c08b7e8bd9ae5ab19005de7f8fbc
abu-umair <mrdwnw93@gmail.com>
2025-11-03 11:02:41 +0700

    ~ hanya delete coment"

--- src/components/ProductForm/ProductForm.tsx
+++ src/components/ProductForm/ProductForm.tsx
@@ -30,10 +30,7 @@ function ProductForm() {
                     placeholder="Nama Produk"
                     labelRequired
                 />
-                {/* <div className="form-group mb-3">
-                    <label className="text-black" htmlFor="product_name">Nama Produk <span className="text-danger">*</span></label>
-                    <input type="text" className="form-control" id="product_name" placeholder="Nama Produk" />
-                </div> */}
+              
 
                 {/* sementara dibiarin karena terkait harga / curency  */}
                 <div className="form-group mb-3">
@@ -51,10 +48,6 @@ function ProductForm() {
                     labelRequired
                 />
 
-                {/* <div className="form-group mb-3">
-                    <label className="text-black" htmlFor="product_image">Gambar Produk <span className="text-danger">*</span></label>
-                    <input type="file" className="form-control" id="product_image" accept="image/*" />
-                </div> */}
 
                 <FormInput<ProductFormValues>
                     errors={form.formState.errors}
@@ -65,10 +58,6 @@ function ProductForm() {
                     placeholder="Deskripsi Produk ..."
                 />
 
-                {/* <div className="form-group mb-4">
-                    <label className="text-black" htmlFor="product_description">Deskripsi</label>
-                    <textarea className="form-control" id="product_description" rows={4} placeholder="Deskripsi produk..."></textarea>
-                </div> */}
 
                 <div className="form-group">
                     <button className="btn btn-primary" type="submit">Simpan Produk</button>
