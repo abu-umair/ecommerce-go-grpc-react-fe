b347221a78334669bf70dff7d1764139d9dae5ec
abu-umair <mrdwnw93@gmail.com>
2025-11-03 15:54:04 +0700

    + proses create poduct

--- src/pages/AdminCreateProduct/AdminCreateProduct.tsx
+++ src/pages/AdminCreateProduct/AdminCreateProduct.tsx
@@ -1,7 +1,59 @@
 import PlainHeroSection from '../../components/PlainHeroSection/PlainHeroSection'
 import ProductForm from '../../components/ProductForm/ProductForm'
+import { type ProductFormValues } from "./../../types/product";
+import useGrpcApi from "../../hooks/useGrpcApi";
+import { getProductClient } from "../../api/grpc/client";
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+
+
+interface uploadImageResponse {
+    file_name: string;
+    message: string;
+    success: boolean;
+}
+
 
 function AdminCreateProduct() {
+    const navigate = useNavigate();
+    const createProductApi = useGrpcApi();
+
+    const submitHandler = async (values: ProductFormValues) => {
+        console.log(values);
+
+        //?upload image menggunakan axios saja
+        const formData = new FormData();
+        formData.append('image', values.image[0]);
+
+        const uploadResponse = await axios.post<uploadImageResponse>('http://localhost:4000/product/upload', formData);
+        if (uploadResponse.status !== 200) {
+            Swal.fire({
+                title: "Upload Gambar Gagal",
+                text: "Silakan coba beberapa saat lagi.",
+                icon: "error",
+            })
+            return
+        }
+
+
+        //?create menggunakan grpc 
+        await createProductApi.callApi(getProductClient().createProduct({
+            description: values.description ?? "",
+            imageFileName: uploadResponse.data.file_name,
+            name: values.name,
+            price: values.price,
+        }));
+
+        Swal.fire({
+            title: "Tambah Produk Sukses",
+            icon: "success",
+        });
+
+        navigate('/admin/products');
+
+    }
+
     return (
         <>
             <PlainHeroSection title='Tambah Produk' />
@@ -10,7 +62,7 @@ function AdminCreateProduct() {
                 <div className="container">
                     <div className="row justify-content-center">
                         <div className="col-md-8">
-                            <ProductForm />
+                            <ProductForm onSubmit={submitHandler} />
                         </div>
                     </div>
                 </div>
