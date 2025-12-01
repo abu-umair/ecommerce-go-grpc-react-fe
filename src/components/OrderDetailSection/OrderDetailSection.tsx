900728405e36ac3683ef1a66cb7f558c534918a7
abu-umair <mrdwnw93@gmail.com>
2025-12-01 11:09:42 +0700

    + mengintegrasikan informasi detail pesanan

--- src/components/OrderDetailSection/OrderDetailSection.tsx
+++ src/components/OrderDetailSection/OrderDetailSection.tsx
@@ -1,6 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import useGrpcApi from "../../hooks/useGrpcApi";
+import { DetailOrderResponse } from "../../../pb/order/order";
+import { getOrderClient } from "../../api/grpc/client";
+import { useEffect, useState } from "react";
 
 function OrderDetailSection() {
+    const { id } = useParams();
+    const detailApi = useGrpcApi();
+    const [apiResponse, setApiResponse] = useState<DetailOrderResponse | null>(null);
+
+    const fetchData = async () => {
+        const res = await detailApi.callApi(getOrderClient().detailOrder({ id: id ?? "" }));
+        setApiResponse(res.response);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
     return (
         <div className="p-4 p-lg-5 border bg-white">
             <Link to="/profile/orders" className="d-inline-block mb-4">
@@ -8,16 +24,16 @@ function OrderDetailSection() {
                     Kembali ke Riwayat
                 </button>
             </Link>
-            <h2 className="section-title mb-4">Pesanan #ORD-2025000002</h2>
+            <h2 className="section-title mb-4">Pesanan  {apiResponse?.number ?? ""}</h2>
 
             <div className="row">
                 <div className="col-md-6 mb-4">
                     <h3 className="h5 mb-3">Informasi Pengiriman</h3>
                     <div className="p-3 border rounded">
-                        <p className="mb-2"><strong>Nama:</strong> Jane Doe</p>
-                        <p className="mb-2"><strong>Telepon:</strong> +6281122334455</p>
-                        <p className="mb-2"><strong>Alamat:</strong> Jl. Melati No. 10, Bandung, Indonesia</p>
-                        <p className="mb-0"><strong>Catatan:</strong> Tolong kirim sore hari</p>
+                        <p className="mb-2"><strong>Nama:</strong> {apiResponse?.userFullName ?? ""}</p>
+                        <p className="mb-2"><strong>Telepon:</strong> {apiResponse?.phoneNumber ?? ""}</p>
+                        <p className="mb-2"><strong>Alamat:</strong> {apiResponse?.address ?? ""}</p>
+                        <p className="mb-0"><strong>Catatan:</strong> {apiResponse?.notes ?? ""}</p>
                     </div>
                 </div>
 
